0b8c0608fd877ea3c1255b4e1d357d6ab82f60d8
ramos <rdiego26@gmail.com>
2026-02-17 19:09:07 +0000

    feat: improve css for 404

--- app/controllers/applets_controller.rb
+++ app/controllers/applets_controller.rb
@@ -9,7 +9,7 @@ class AppletsController < ApplicationController
 
   def applet_not_found
     respond_to do |format|
-      format.html { render "applets/not_found", status: :not_found, layout: "application" }
+      format.html { render "applets/not_found", status: :not_found, layout: false }
       format.json { render json: { error: "Applet not found", message: "Couldn't find Applet with id=#{params[:id]}" }, status: :not_found }
     end
   end
