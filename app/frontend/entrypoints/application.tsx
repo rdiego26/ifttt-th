844ef23937d7bc2f1a2f3f799cb8e2079304ef30
ramos <rdiego26@gmail.com>
2026-02-17 10:14:01 +0000

    refactor: extract styles into css files

--- app/frontend/entrypoints/application.tsx
+++ app/frontend/entrypoints/application.tsx
@@ -4,6 +4,7 @@ import AppletDetail from "../components/AppletDetail"
 import AppletList from "../components/AppletList"
 import { Applet } from "../graphql/queries"
 import "../styles/application.css"
+import "../styles/home.css"
 
 document.addEventListener("DOMContentLoaded", () => {
   // Mount AppletDetail component
---
+++ app/frontend/entrypoints/not_found.ts
@@ -0,0 +1,1 @@
+import "../styles/not_found.css"
